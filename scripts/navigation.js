c0aaa3f6f29dad10b79938de0724f24e2482e6fa
fredric47 <lpratt1@byupathway.edu>
2026-03-16 14:44:28 -0600

    ham button

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,8 +1,8 @@
 const navbutton = document.querySelector('#ham-btn');
+const navbar = document.querySelector('#nav-bar');
 
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle('show');
     navbar.classList.toggle('show');
 });
 
-const navbar = document.querySelector('#nav-bar');
