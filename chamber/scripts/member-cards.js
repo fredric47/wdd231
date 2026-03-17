4c532acd9fdad21acc05168b449dc078ad3f813a
fredric47 <lpratt1@byupathway.edu>
2026-03-17 16:11:48 -0600

    flex

--- chamber/scripts/member-cards.js
+++ chamber/scripts/member-cards.js
@@ -2,15 +2,16 @@ const url = 'https://raw.githubusercontent.com/fredric47/wdd231/refs/heads/main/
 const cards = document.querySelector('#cards');
 
 async function getMemberData() {
-    const response = await fetch(url)
+    const response = await fetch(url);
     const data = await response.json();
     //console.table(data.prophets); //temporary testing of data response
-    displayMembers(data.members);
+    displayMembers(data); //used copilot to help remove .members
 }
 
 const displayMembers = (members) => {
     members.forEach(member => {
         let card = document.createElement('section');
+        // card.classList.add('visible');  //Used copilot to figure this out
         let name = document.createElement('h2');
         let address = document.createElement('span');
         let phoneNo = document.createElement('span');
@@ -18,15 +19,15 @@ const displayMembers = (members) => {
         let image = document.createElement('img');
         let membershipLevel = document.createElement('span');
 
-        name.textContent = `-${member.companyName}`;
-        address.textContent = `-${member.companyAddress}`;
-        phoneNo.textContent = `#${member.companyPhoneNo}`;
-        url.textContent = `-${member.companyURL}`;
+        name.innerHTML = `${member.companyName}`;
+        address.innerHTML = `<span class="bold">Address</span>: ${member.companyAddress}`;
+        phoneNo.innerHTML = `<span class="bold">Phone#</span> ${member.companyPhoneNo}`;
+        url.innerHTML = `<span class="bold">Web Address:</span> ${member.companyURL}`;
         // image.src = `images/${members.companyImg}`;
-        membershipLevel.textContent = `Membership Lv: ${member.membershipLevel}`;
+        membershipLevel.innerHTML = `<span class="bold">Membership Lv:</span> ${member.membershipLevel}`;
 
         image.setAttribute('alt', `${member.companyName} company image`);
-        image.setAttribute('src', `images/${members.companyImg}`);
+        image.setAttribute('src', `images/${member.companyImg}`);
         image.setAttribute('loading', 'lazy');
         image.setAttribute('width', '340');
         image.setAttribute('height', '440');
--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,7 +1,17 @@
 const hamButton = document.querySelector('#ham-btn');
 const navBar = document.querySelector('#nav-bar');
 
+const listButton = document.querySelector('#lightdark')
+
 hamButton.addEventListener('click', () => {
     hamButton.classList.toggle('show');
     navBar.classList.toggle('show');
 });
+
+listButton.addEventListener('click', () => {
+    document.querySelectorAll('#cards section').forEach(card => {  //used copilot to figure this out
+        card.classList.toggle('notVisible');
+    });
+    document.querySelector('#cards').classList.toggle('list');
+});
+
