1b23f378f8d5168a93954b03bd2cb1cd15f2529b
fredric47 <lpratt1@byupathway.edu>
2026-04-08 12:06:30 -0600

    template-area

--- chamber/data/items_of_interest.mjs
+++ chamber/data/items_of_interest.mjs
@@ -11,49 +11,64 @@ export const itemsOfInterest = [
         "name": "Goat Fields",
         "address": "321N 123S Goats in the Field Rd",
         "description": "Not everywhere can you find a field of goats with a bunch of baby goats! Great place to see both",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/goat-field.webp?raw=true"
+        "image": "images/goat-field.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/goat-field.webp?raw=true
     },
 
     {
         "name": "Poppy the Cat",
         "address": "146N 642S Duck Marsh",
         "description": "Ever wanted to see a cat that was completly unique and unlike anyother ever? We've got one! (Just try and prove us wrong).",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/cat.webp?raw=true"
+        "image": "images/cat.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/cat.webp?raw=true
     },
 
     {
         "name": "Clasic bus",
         "address": "510N 015S Trauma Lane",
         "description": "Come see our non clasic bus today and relive the trauma of your childhood.",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/bus.webp?raw=true"
+        "image": "images/bus.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/bus.webp?raw=true
     },
 
     {
         "name": "Cute Baby Goats",
         "address": "725N 527S Baby Bolivard",
         "description": "Baby goats are about as cute as they come, come visit us today.",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/baby-goat.webp?raw=true"
+        "image": "images/baby-goat.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/baby-goat.webp?raw=true
     },
 
     {
         "name": "Ye Old Volcano",
         "address": "51N 15S Firey End Trail",
         "description": "We have an old dead volcano. Come see a grand landmark from the past",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/volcano.webp?raw=true"
+        "image": "images/volcano.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/volcano.webp?raw=true
     },
 
     {
         "name": "Redneck B&B",
         "address": "98N 89S Center of Nowhere",
         "description": "Come and spend a night in our fine redneck B&B. All bedbugs are complementary and don't add to your price.",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/tiny-house.webp?raw=true"
+        "image": "images/tiny-house.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/tiny-house.webp?raw=true
+    },
+
+    {
+        "name": "The Three Blind Mice",
+        "address": "12N 21S They Run St",
+        "description": "Direct decendants of the origional three blind mice! ",
+        "image": "images/bmice.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/bmice.webp?raw=true
     },
 
     {
         "name": "Natural Gardin",
         "address": "420N 0S Natural Gardins Rd",
         "description": "Come see ground that has been around for centeries and enjoy the plants grown here",
-        "image": "https://github.com/fredric47/wdd231/blob/main/chamber/images/natural-garden.webp?raw=true"
+        "image": "images/natural-garden.webp"
+        // https://github.com/fredric47/wdd231/blob/main/chamber/images/natural-garden.webp?raw=true
     }
 ];
 
