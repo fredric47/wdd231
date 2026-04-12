c530cdb2a8e0d9cad2b31d8c5dd3567a0ae14e82
fredric47 <lpratt1@byupathway.edu>
2026-04-11 21:19:51 -0600

    members

---
+++ finalproject/data/crew_list.mjs
@@ -0,0 +1,30 @@
+export const artemis2Crew = [
+    {
+        name: "Reid Wiseman",
+        age: 50,               // Born 1975
+        Born: 1975,
+        position: "Commander",
+        gender: "Male"
+    },
+    {
+        name: "Victor Glover",
+        age: 49,               // Born 1976
+        Born: 1976,
+        position: "Pilot",
+        gender: "Male"
+    },
+    {
+        name: "Christina Koch",
+        age: 47,               // Born 1979
+        Born: 1979,
+        position: "Mission Specialist",
+        gender: "Female"
+    },
+    {
+        name: "Jeremy Hansen",
+        age: 50,               // Born 1976
+        Born: 1976,
+        position: "Mission Specialist",
+        gender: "Male"
+    }
+];
---
+++ finalproject/scripts/artemis_members.js
@@ -0,0 +1,4 @@
+import {list} from '../data/crew_list.mjs'
+console.log(list)
+
+const crew = document.querySelector('#orion-crew');
